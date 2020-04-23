74ed6559684d0428f5abb57cf736fd198a4e7493
tcarroll2 <tc98207@gmail.com>
2020-04-23 12:34:02 -0500

    Initial Commit

---
+++ Project9-tc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project9-tc
+//
+//  Created by Thomas Carroll on 4/23/20.
+//  Copyright © 2020 Thomas Carroll. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project9-tc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project9-tc
+//
+//  Created by Thomas Carroll on 4/23/20.
+//  Copyright © 2020 Thomas Carroll. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
